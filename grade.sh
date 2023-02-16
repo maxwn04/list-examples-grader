405e09e6f8f72f62f232e38bc5da7a13cb8a3fae
iAmWeirdTheFifth <maxwn04@gmail.com>
2023-02-16 10:19:04 -0800

    check files

--- grade.sh
+++ grade.sh
@@ -2,4 +2,12 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+if[[-f ListExamples.java]]
+then
+    echo "Files Found"
+else
+    echo "Files not Found"
+    exit
+fi
+echo "Finished cloning"
+cp student-submission/ListExamples.java `pwd`
